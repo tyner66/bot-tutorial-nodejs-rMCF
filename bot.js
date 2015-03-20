80ccca2c6b9f2abfb0500e4ccff812cfb12f4521
tyner66 <pearsontyler57@gmail.com>
2015-03-20 11:56:16 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -83,7 +83,7 @@ function respond() {
   } 
   else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/SNKLFRTZ/team/"+request.text.substring(5,8)+"/depthchart");
+    postMessage("http://daddyleagues.com/mml/team/"+request.text.substring(5,8)+"/depthchart");
     this.res.end();
   } 
   else if(request.text && botRegexSalt.test(request.text)) {
@@ -103,14 +103,14 @@ function respond() {
   } 
   else if(request.text && botRegexSC.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/SNKLFRTZ/team/"+request.text.substring(5,8)+"/schedule");
+    postMessage("http://daddyleagues.com/mml/team/"+request.text.substring(5,8)+"/schedule");
     this.res.end();
   }
   else if(request.text && botRegexP.test(request.text)) {
     this.res.writeHead(200);
     var req = request.text.substring(5,request.text.length);
     var rep = req.replace(/ /,"+");
-    postMessage("http://daddyleagues.com/SNKLFRTZ/players?name="+rep+"&position=all&team=all");
+    postMessage("http://daddyleagues.com/mml/players?name="+rep+"&position=all&team=all");
     this.res.end();
   } 
 
