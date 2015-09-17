6712f3a30b4da9288dd1e3357324b45aae6e0155
tyner66 <pearsontyler57@gmail.com>
2015-09-17 14:47:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/cool guy/; botRegexNW = /^\/nw/; botRegexNO = /^\/no/; botRegexKISHK = /^\/kishk/; botRegexSOS = /^\/sos/; botRegexFUCK = /^\/idgaf/; botRegexSHOTS = /^\/shots fired/; botRegexDAMN = /^\/damn/; botRegexMIND = /^\/mind blown/; botRegexSORRY = /^\/sorry/; botRegexOK = /^\/ok/; botRegexLOL = /^\/lol/; botRegexARCHER = /^\/archer/; botRegexPLAYER = /^\/player stats/; botRegexSTATS = /^\/team stats/; botRegexGAMES = /^\/games/; botRegexSTANDINGS = /^\/standings/; botRegexPOWER = /^\/power/; botRegexSTATEMENT = /^\/statement/; botRegexTRADES =/^\/trades/;  botRegexOWNERS = /^\/owner/; botRegexDADDY = /^\/dl/; botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexYub = /^\/yubnub/
+      botRegex = /^\/cool guy/; botRegexARCH = /^\/my ass/; botRegexEYES = /^\/eyes/; botRegexKNIGHT = /^\/yeah/; botRegexNW = /^\/nw/; botRegexNO = /^\/no/; botRegexKISHK = /^\/kishk/; botRegexSOS = /^\/sos/; botRegexFUCK = /^\/idgaf/; botRegexSHOTS = /^\/shots fired/; botRegexDAMN = /^\/damn/; botRegexMIND = /^\/mind blown/; botRegexSORRY = /^\/sorry/; botRegexOK = /^\/ok/; botRegexLOL = /^\/lol/; botRegexARCHER = /^\/archer/; botRegexPLAYER = /^\/player stats/; botRegexSTATS = /^\/team stats/; botRegexGAMES = /^\/games/; botRegexSTANDINGS = /^\/standings/; botRegexPOWER = /^\/power/; botRegexSTATEMENT = /^\/statement/; botRegexTRADES =/^\/trades/;  botRegexOWNERS = /^\/owner/; botRegexDADDY = /^\/dl/; botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexYub = /^\/yubnub/
       botRegexAd=/^\/advance/; botRegexSC = /^\/SDL/i;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; 
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -14,6 +14,21 @@ function respond() {
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
     postMessage(cool());
+    this.res.end();
+  }
+  else if(request.text && botRegexARCH.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://media.giphy.com/media/UwkyBeoRq3G5W/giphy.gif");
+    this.res.end();
+  }
+  else if(request.text && botRegexEYES.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://media.giphy.com/media/feqkVgjJpYtjy/giphy.gif");
+    this.res.end();
+  }
+  else if(request.text && botRegexKNIGHT.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage("https://media.giphy.com/media/mcxZ7EAG0nQFW/giphy.gif");
     this.res.end();
   }
   else if(request.text && botRegexNW.test(request.text)) {
@@ -28,12 +43,12 @@ function respond() {
   }
   else if(request.text && botRegexKISHK.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://giphy.com/gifs/maddengiferator-cardinals-arizona-larry-fitzgerald-5wWf7H6s5NgBvofUA00");
+    postMessage("https://media.giphy.com/media/e4qfyCfJqoRxK/giphy.gif");
     this.res.end();
   }
   else if(request.text && botRegexSOS.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://www.daddyleagues.com/MML16/schedules/strength");
+    postMessage("http://www.daddyleagues.com/MML/schedules/strength");
     this.res.end();
   }
   else if(request.text && botRegexFUCK.test(request.text)) {
@@ -78,27 +93,27 @@ function respond() {
   }
   else if(request.text && botRegexPLAYER.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://www.daddyleagues.com/MML16/stats/player");
+    postMessage("http://www.daddyleagues.com/MML/stats/player");
     this.res.end();
   }
   else if(request.text && botRegexSTATS.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://www.daddyleagues.com/MML16/stats/team");
+    postMessage("http://www.daddyleagues.com/MML/stats/team");
     this.res.end();
   }
   else if(request.text && botRegexGAMES.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://www.daddyleagues.com/MML16/schedules");
+    postMessage("http://www.daddyleagues.com/MML/schedules");
     this.res.end();
   }
   else if(request.text && botRegexSTANDINGS.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://www.daddyleagues.com/MML16/standings/league");
+    postMessage("http://www.daddyleagues.com/MML/standings/league");
     this.res.end();
   }
   else if(request.text && botRegexPOWER.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://www.daddyleagues.com/MML16/standing/ranking");
+    postMessage("http://www.daddyleagues.com/MML/standing/ranking");
     this.res.end();
   } 
   else if(request.text && botRegexSTATEMENT.test(request.text)) {
@@ -118,12 +133,12 @@ function respond() {
   } 
   else if(request.text && botRegexDADDY.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/mml16");
+    postMessage("http://daddyleagues.com/mml");
     this.res.end();
   } 
   else if(request.text && botRegexDL.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/mml16/team/"+request.text.substring(5,8)+"/depthchart");
+    postMessage("http://daddyleagues.com/mml/team/"+request.text.substring(5,8)+"/depthchart");
     this.res.end();
   } 
   else if(request.text && botRegexSalt.test(request.text)) {
